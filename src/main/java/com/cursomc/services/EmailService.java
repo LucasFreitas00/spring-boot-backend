de89bba55097f05ed3fbe8831bc9445d005f8f72
LucasFreitas00 <lucasdsf369@gmail.com>
2022-01-15 15:26:55 -0300

    Projeto finalizado e documentado com Swagger

---
+++ src/main/java/com/cursomc/services/EmailService.java
@@ -0,0 +1,21 @@
+package com.cursomc.services;
+
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.mail.SimpleMailMessage;
+
+import com.cursomc.domain.Cliente;
+import com.cursomc.domain.Pedido;
+
+public interface EmailService {
+
+	void sendOrderConfirmationEmail(Pedido obj);
+	
+	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
+}
